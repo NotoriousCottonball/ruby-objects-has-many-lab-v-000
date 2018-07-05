ea9c97f5ffa8d05c3f1c23a99eae8e252fde428d
George  Pianka <gpianka1@gmail.com>
2018-07-05 03:01:35 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,5 +15,11 @@ class Author
   end 
   
   def add_post_by_title(title)
-    post
+    post = Post.new(title) 
+    @posts << post 
+    post.author = self 
+    @@post_count += 1 
+  end 
+  
+  def 
     
