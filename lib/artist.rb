46f3fe9e8e3ab19bda9506bb030262553aeb511c
George  Pianka <gpianka1@gmail.com>
2018-07-05 02:41:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,10 @@ class Artist
     
   def add_song_by_name(name)
     song = Song.new(name) 
-    
+    @songs << song 
+    song.artist = self 
+    @@song_count += 1
+  end
     
   def self.song_count
   
